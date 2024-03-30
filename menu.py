5d81737be1364c455329c9144a0d2b8af49f2f80
RubenMaestre <formacion@rubenmaestre.com>
2024-03-30 13:09:56 +0100

    actualiza

--- menu.py
+++ menu.py
@@ -1,13 +1,16 @@
+# menu.py
 import streamlit as st
 
 # Importa tus páginas aquí
-from pages import home, overview, players, teams, matches
+from pages import home, overview, teams, players, matches
+
+# Asegúrate de que cada módulo de página tenga una función 'display'
+# Por ejemplo, en home.py deberías tener algo como:
+# def display():
+#     st.title('Inicio')
+#     # Resto del contenido de la página de inicio
 
 def create_sidebar():
-    # Insertar CSS personalizado desde un archivo separado
-    with open('styles/custom_styles.css') as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
     # Define los ítems del menú con las páginas que tienes
     menu_items = {
         "🏠 Inicio": home.display,
@@ -17,7 +20,21 @@ def create_sidebar():
         "🏟 Partidos": matches.display,
     }
 
+    # Insertar CSS personalizado desde un archivo separado
+    with open('styles/custom_styles.css') as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+    # Configuración inicial de la página activa
+    if 'active_page' not in st.session_state:
+        st.session_state['active_page'] = '🏠 Inicio'
+
     st.sidebar.title("Menú")
     for title, page_func in menu_items.items():
         if st.sidebar.button(title):
-            page_func()
+            st.session_state['active_page'] = title  # Guarda el título de la página activa
+
+    # Ejecutar la función de la página activa
+    menu_items[st.session_state['active_page']]()
+
+# Ahora, llamar a create_sidebar en app.py, no aquí.
+
