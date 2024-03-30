6233014e615ba26ac6b1acf4566ebb1321bcba09
RubenMaestre <formacion@rubenmaestre.com>
2024-03-30 13:16:06 +0100

    actualizar

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 import streamlit as st
 from menu import create_sidebar
 
-# Configuraciones globales de la app, como el título y la configuración de la página
-st.set_page_config(page_title="Mi Proyecto Streamlit")
-
-# Puedes llamar aquí a cualquier configuración global adicional si es necesario
-
 # Llama a la función de la barra lateral que crea el menú
 create_sidebar()
