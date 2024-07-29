1ea377c76dea49f3649898a7a40d75943777f2aa
RubenMaestre <formacion@rubenmaestre.com>
2024-07-29 13:43:55 +0200

    Update overview.py

--- paginas/overview.py
+++ paginas/overview.py
@@ -27,19 +27,14 @@ def display():
 
     st.markdown("---")
 
-    # Dividir la interfaz en dos columnas: una para la navegación y otra para el contenido
-    col_navegacion, divider, col_contenido = st.columns([1, 0.3, 7])
-    with col_navegacion:
-        st.markdown("<h4 style='text-align: center;'>Pulse una opción para saber más</h4>", unsafe_allow_html=True)
-        
-        # Navegación dentro de la competición seleccionada
-        if competicion == "Primera División":
-            primera_division.display(año)
-        elif competicion == "Segunda División":
-            segunda_division.display(año)
-        elif competicion == "Copa del Rey":
-            copa_del_rey.display(año)
-        elif competicion == "Supercopa":
-            supercopa.display(año)
+    # Navegación dentro de la competición seleccionada
+    if competicion == "Primera División":
+        primera_division.display(año)
+    elif competicion == "Segunda División":
+        segunda_division.display(año)
+    elif competicion == "Copa del Rey":
+        copa_del_rey.display(año)
+    elif competicion == "Supercopa":
+        supercopa.display(año)
 
 display()
